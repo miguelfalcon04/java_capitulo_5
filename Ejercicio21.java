4a40b492ed79a7f22a55b894a0f51f7b9e82bb0e
miguelfal <falconmiguel.2004@gmail.com>
2022-10-29 19:12:24 +0200

    commit

--- Ejercicio21.java
+++ Ejercicio21.java
@@ -2,30 +2,31 @@ import java.util.Scanner;
 public class Ejercicio21 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int nintroducido;
-        int nelemento=0;
-        int nelementoimpar=0;
-        int simpar=0;
+        int n;
+        int cant=0;
+        float cantimp=0;
         int mpar=0;
-        System.out.println("Introduce un numero negativo para terminar con el bucle: ");
+        float sumaimp=0;
         do{
-            nintroducido = s.nextInt();
-            if (nintroducido>0){
-                nelemento++;
-            }
-            if ((nintroducido%2)==1){
-                simpar+=nintroducido;
-                nelementoimpar+=1;
-            }
-            else{
-                if(nintroducido>mpar){
-                    mpar=nintroducido;
+            System.out.print("Introduzca un numero: ");
+            n=s.nextInt();
+            if(n>0){
+                if(n%2!=0){
+                    cant=cant+1;
+                    cantimp=cantimp+1;
+                    sumaimp=sumaimp+n;
+                }
+                else
+                if(n%2==0 && n>mpar){
+                    mpar=n;
+                    cant=cant+1;
                 }
             }
-        } while (nintroducido>0);
-        System.out.println("Se han introducido "+nelemento+ " numeros");
-        System.out.println("La media de los numeros impares es: "+(simpar/nelementoimpar));
-        System.out.println("El mayor de los pares es "+mpar);
-        s.close();
+        }while(n>=0);
+        System.out.println("Se han introducido "+cant+" numeros");
+        System.out.printf("La media de los numeros impares es: %.2f\n",sumaimp/cantimp);
+        System.out.println("El mayor de los numeros pares es: "+mpar);
+    s.close();
     }
 }
+
