cf3c30ef3f24cfaf482acac50403e0c67a25887e
miguelfal <falconmiguel.2004@gmail.com>
2022-11-03 23:19:41 +0100

    commit

--- Ejercicio27.java
+++ Ejercicio27.java
@@ -7,7 +7,7 @@ public class Ejercicio27 {
         int aux=0;
         int i=0;
 
-        System.out.printf("Los multiplos de que hay entre 1 y %d son ",n);
+        System.out.printf("Los multiplos de 3 que hay entre 1 y %d son ",n);
         for(i=0; i<=n;i+=3){
             aux=aux+i;
             System.out.print(i+" ");
