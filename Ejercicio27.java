a420968cfdcda82672f5192e58f51195b444076a
miguelfal <falconmiguel.2004@gmail.com>
2022-11-03 14:04:33 +0100

    commit

---
+++ Ejercicio27.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Ejercicio27 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Introduzca un numero: ");
+        int n =s.nextInt();
+        int aux=0;
+        int i=0;
+
+        System.out.printf("Los multiplos de que hay entre 1 y %d son ",n);
+        for(i=0; i<=n;i+=3){
+            aux=aux+i;
+            System.out.print(i+" ");
+        }
+
+        System.out.println("");
+        System.out.println("Suman en total "+aux);
+    s.close();
+    }
+}
