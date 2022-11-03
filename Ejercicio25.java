c9b6a4e4d61ca85297bbe5945ff77e521fb54ea8
miguelfal <falconmiguel.2004@gmail.com>
2022-11-03 08:42:29 +0100

    commit

--- Ejercicio25.java
+++ Ejercicio25.java
@@ -3,11 +3,14 @@ public class Ejercicio25 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.print("Introduzca el numero para darle la vuelta: ");
-        int n = s.nextInt();
-        while(n>=1){
-            System.out.print(n%10);
+        long n = s.nextLong();
+        long volteado=0;
+        long aux=n;
+        while(n>0){
+            volteado=volteado*10+n%10;
             n=n/10;
         }
+    System.out.printf("El nuemro introducido %d volteado es %d",aux,volteado);
     s.close();
     }
 }
