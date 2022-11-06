8db1fd49343ad4903f50ad9844d88f246e9ab4fd
miguelfal <falconmiguel.2004@gmail.com>
2022-11-06 18:49:59 +0100

    commit

--- Ejercicio36.java
+++ Ejercicio36.java
@@ -5,29 +5,20 @@ public class Ejercicio36 {
         System.out.print("Introduzca su numero para comprobar que es capicua: ");
         int n = sc.nextInt();
         int nvolteado=0;
-        boolean capicua=true;
+        int save=n;
 
         while(n>0){
             nvolteado=nvolteado*10+(n%10);
             n=n/10;
         }
-        System.out.print(nvolteado);
 
-        do{
-            if(n == nvolteado){
-                capicua=true;
-                n=n/10;
-                nvolteado=nvolteado/10;
+            if(save == nvolteado){
+                System.out.print("Es capicúa");
             }
             else{
                 System.out.print("No es capicua.");
-                capicua=false;
             }
-        }while(capicua);
-        
-        if(capicua){
-            System.out.printf("El numero %d es capicua.",n);
-        }
+
     sc.close();
     }
 }
