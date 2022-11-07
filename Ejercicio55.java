4a09e1ec88352c63e4d77105bb55d1a512646dd6
miguelfal <falconmiguel.2004@gmail.com>
2022-11-07 10:17:11 +0100

    commit

---
+++ Ejercicio55.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Ejercicio55 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduzca un numero: ");
+        int n = sc.nextInt();
+        int save=n;
+        int aux=n;
+
+        int nvolteado=0;
+        int longitud=0;
+        int digito=0;
+
+        while(n>0){
+            nvolteado=nvolteado*10+(n%10);
+            longitud++;
+            n/=10;
+        }
+        
+        if(longitud>3){
+            nvolteado=nvolteado%10;
+            nvolteado/=10;
+            aux=aux%10;
+            double op= Math.pow(10, longitud-1);
+            save=save-((int)op*digito);
+
+            System.out.println("El número resuelto es "+aux+""+save/10);
+        }
+
+        if(longitud==2){
+            System.out.println("El número resuelto es "+nvolteado);
+        }
+
+        if(longitud==1){
+            System.out.println("El número resuelto es "+save);
+        }
+
+    sc.close();
+    }
+}
